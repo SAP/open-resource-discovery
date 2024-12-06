96b46e2377f7248b31204f8ea8e4c5a400e1a9e9
Fannon <heimlersimon@gmail.com>
2024-12-06 08:46:55 +0000

    Deploying to gh-pages from @ SAP/open-resource-discovery@2d616e4cd270786dc04984ea09836245747bf905 🚀

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,25 @@
-function locationHashChanged(){document.querySelectorAll(".highlight").forEach((t=>{t.classList.remove("highlight")}));const t=location.hash.split("#")[1];console.debug("highlighting",t);const e=document.getElementById(t);e&&(e.classList.add("highlight"),e.getElementsByTagName("a")[0]?addAnchorTitle(e.getElementsByTagName("a")[0].title):addAnchorTitle(e.textContent))}function addAnchorTitle(t){const e=document.title.split(" | ");t=t.replace("Direct link to ",""),document.title=`${t} | ${e[e.length-1]} | ${e[e.length-2]}`}window.addEventListener("load",locationHashChanged,!1),window.addEventListener("hashchange",locationHashChanged,!1);
+// Little script to highlight the links and definitions that were navigated to
+window.addEventListener('load', locationHashChanged, false)
+window.addEventListener('hashchange', locationHashChanged, false)
+function locationHashChanged() {
+  document.querySelectorAll('.highlight').forEach((dfn) => {
+    dfn.classList.remove('highlight')
+  })
+  const highlightedElementId = location.hash.split('#')[1]
+  console.debug('highlighting', highlightedElementId)
+  const highlightedElement = document.getElementById(highlightedElementId)
+  if (highlightedElement) {
+    highlightedElement.classList.add('highlight')
+    if (highlightedElement.getElementsByTagName('a')[0]) {
+      addAnchorTitle(highlightedElement.getElementsByTagName('a')[0].title)
+    } else {
+      addAnchorTitle(highlightedElement.textContent)
+    }
+  }
+}
+
+function addAnchorTitle(anchorTitle) {
+  const split = document.title.split(' | ')
+  anchorTitle = anchorTitle.replace('Direct link to ', '')
+  document.title = `${anchorTitle} | ${split[split.length - 1]} | ${split[split.length - 2]}`
+}
